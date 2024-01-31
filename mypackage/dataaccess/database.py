d8eee6e8cc357ec7d86d1e5177b775c7855e83a6
piyota0901 <piyo.tatsu.0901@gmail.com>
2024-01-31 21:16:08 +0900

    chore: remove files in my packages for changing langchain based

--- mypackage/dataaccess/database.py
+++
@@ -1,8 +0,0 @@
-import os
-from qdrant_client import QdrantClient
-
-def get_qdrant_client():
-    """Create a QdrantClient instance
-    """
-    url = "http://" + os.environ["QDRANT_HOST"] + ":" + os.environ["QDRANT_PORT"]
-    return QdrantClient(url=url)
--- mypackage/service/interfaces.py
+++
@@ -1,6 +0,0 @@
-import abc
-
-class IStartupService(abc.ABC):
-    @abc.abstractmethod
-    def find_startup(self, id: int) -> list[]:
-        pass
